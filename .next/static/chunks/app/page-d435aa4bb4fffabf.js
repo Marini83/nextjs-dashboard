231bbcae7e69222fec8f4ef26d6440ac5f1262df
Marini83 <kallashi@gmail.com>
2024-03-05 15:42:05 -0500

    fixed data.tsx

--- .next/static/chunks/app/page-d435aa4bb4fffabf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{5597:function(e,n,s){Promise.resolve().then(s.t.bind(s,5029,23)),Promise.resolve().then(s.t.bind(s,1749,23)),Promise.resolve().then(s.t.bind(s,5250,23))},5029:function(e){e.exports={shape:"home_shape__hPvH_"}}},function(e){e.O(0,[250,749,971,69,744],function(){return e(e.s=5597)}),_N_E=e.O()}]);
